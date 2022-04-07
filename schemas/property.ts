c605467aa4e3bcff4519bb6f8a6ab70d826b6079
Neel007x <neelunstp@gmail.com>
2022-04-07 13:20:12 +0530

    Initial Sanity setup

---
+++ schemas/property.ts
@@ -0,0 +1,12 @@
+export default {
+  title: "Property",
+  name: "property",
+  type: "document",
+  fields: [
+    {
+      title: "Location",
+      name: "location",
+      type: "string",
+    },
+  ],
+};
